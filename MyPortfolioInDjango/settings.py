6e3248dd13d1eb69dceb0018459fb4569bf2b19d
Dilip  Sahu <60321342+dilipksahu@users.noreply.github.com>
2020-10-18 22:27:20 +0530

    Removed password and email for security reason

--- MyPortfolioInDjango/settings.py
+++ MyPortfolioInDjango/settings.py
@@ -133,6 +133,6 @@ MESSAGE_TAGS = {
 # Email Sending
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sahudil418@gmail.com'
-EMAIL_HOST_PASSWORD = 'Itvedant@0202'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
